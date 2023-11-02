f179162abc9f640f11b07f2bdadfe6736cd0cc34
yh-project <yoha6865@gmail.com>
2023-11-03 02:27:01 +0900

    Feat/#4: SideBar 구현
    Material Tailwind CSS Drawer 이용
     - 특정 메뉴 클릭 시 해당 페이지로 이동
     - 현재는 예시로  carstate 로 이동시켜둠
    
    App.js
     - 예시로 사용하기 위해 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import "./App.css";
+import SideBar from "components/SideBar";
+import CarState from "pages/CarState/CarState";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center w-screen h-screen">
-        <h1 className="text-[48px] font-bold">Notice</h1>
-        <p className="text-2xl font-bold">
-          이전의 모든 세팅들을 그대로 들고 옴. package.json의 name은 수정.
-        </p>
-        <p className="text-2xl font-bold">
-          .github 내부 workflow는 문제생길까봐 모두 주석처리하였음.
-        </p>
-        <p className="text-2xl font-bold">
-          모든 폴더 내부에는 .gitkeep 파일이 있으므로 사용시 삭제
-        </p>
-        <p className="text-2xl font-bold">
-          pakage랑 yarn lock 다 붙여와서 그냥 한건데.. 안꼬이겠지..
-        </p>
-      </div>
+      {/* 사이드바 */}
+      <SideBar />
+      {/* 메인 컨텐츠 */}
+      <Routes>
+        <Route path="/carstate" element={<CarState />}></Route>
+      </Routes>
     </>
   );
 }
