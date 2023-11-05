eff5a26226a34cbdec76cc8b07f2d01825ea9f97
seolbotdae <seolbotdae@gmail.com>
2023-11-05 14:01:47 +0900

    Fix/#8: 주차장 컴포넌트 위치 이동과 UI 수정
    주차장 컴포넌트 위치 이동
    
    상단의 메뉴 제목과 본문 간의 거리 줄임

--- src/pages/MapManage/MapManage.js
+++ src/pages/MapManage/MapManage.js
@@ -1,11 +1,11 @@
-import ParkingMap from "components/ParkingMap/ParkingMap";
+import ParkingMap from "pages/MapManage/ParkingMap/ParkingMap";
 import React from "react";
 
 const MapManage = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <div className="w-[1140px] flex flex-col justify-center items-center mb-10">
-        <div className="text-[50px] font-bold self-start mb-10 mt-[130px]">
+        <div className="text-[50px] font-bold self-start mt-[80px]">
           주차장 관리
         </div>
         <ParkingMap></ParkingMap>
