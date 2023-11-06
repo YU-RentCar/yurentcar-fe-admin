744bf091601c6911d2dd7717c387f0b95fd9e6ca
yh-project <yoha6865@gmail.com>
2023-11-06 10:30:07 +0900

    Feat/#10: 항목 이름 한글화
    항목 이름을 한글화하기위한 객체

---
+++ src/pages/ManageCar/title.js
@@ -0,0 +1,19 @@
+export const title = {
+  carName: "차종",
+  carNumber: "차 번호",
+  totalDistance: "총 주행거리",
+  afterPrice: "할인 후",
+  discountRate: "할인율",
+  discountReason: "할인 사유",
+  carDescription: "차량 설명",
+  oilType: "유종",
+  carSize: "차량 크기",
+  releaseDate: "출시일",
+  createdAt: "등록일",
+  maxPassenger: "승차 인원",
+  transmission: "구동기",
+  carBrand: "브랜드",
+  isKorean: "국산/외제",
+};
+
+export default title;
