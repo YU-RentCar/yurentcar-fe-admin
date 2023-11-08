b3e179ab11cffb3fbd0dae46ef95608ee6ce417d
yh-project <yoha6865@gmail.com>
2023-11-08 20:14:07 +0900

    Fix/#17: maxPage 운용 방식 변경
    같은 값일 경우 렌더링 안되는 문제 해결
     - 단순 Number 가 아닌 객체로 관리

--- src/pages/Key/Key.js
+++ src/pages/Key/Key.js
@@ -1,5 +1,8 @@
 import Normal from "./Normal";
 import Modify from "./Modify";
+import Alert from "popUp/Alert";
+import { useRecoilValue } from "recoil";
+import { alertAtom } from "recoil/alertAtom";
 import { useEffect, useState } from "react";
 import { useKey } from "./utils/useKey";
 import {
@@ -17,20 +20,22 @@ import {
 
 const Key = () => {
   const ku = useKey(); // Key 의 Utils
+  const alertInfo = useRecoilValue(alertAtom); // alert 제어
   const [page, setPage] = useState(1); // 현재 페이지
-  const [maxPage, setMaxPage] = useState(1); // 최대 페이지
+  const [maxPage, setMaxPage] = useState({ num: 1 }); // 최대 페이지
   const [filterMenu, setFilterMenu] = useState([
     "전체",
-    "사용가능",
-    "수리/점검",
+    "마스터키",
+    "사용중",
+    "여분키",
     "도난",
   ]); // 검색 필터
-
   const [six, setSix] = useState([]); // 실제 화면 상에 보여질 6개
   const [searchTarget, setSearchTarget] = useState(""); // 검색할 차량 번호
+  const [mIdx, setMIdx] = useState(-1); // 현재 수정 중인 차 키 index
   useEffect(() => {
     const tmp = [...ku.getKeyList()]; // 서버로부터 차량 리스트 조회
-    setMaxPage(Math.ceil(tmp.length / 6));
+    setMaxPage({ num: Math.ceil(tmp.length / 6) });
   }, []);
   // maxPage 가 바뀌면 -> 새로운 데이터셋
   useEffect(() => {
@@ -72,10 +77,10 @@ const Key = () => {
                     return (
                       <MenuItem
                         className="flex items-center justify-center text-lg font-bold"
-                        onClick={() => {
-                          const target = document.getElementById("sort");
-                          ku.changeMenu("title", target, filterMenu[i], 0);
-                        }}
+                        onClick={() =>
+                          (document.getElementById("sort").innerText =
+                            filterMenu[i])
+                        }
                         key={i}
                       >
                         {v}
@@ -92,7 +97,7 @@ const Key = () => {
                     searchTarget,
                     document.getElementById("sort").innerText
                   );
-                  setMaxPage(Math.ceil(tmp.length / 6)); // 검색 결과 -> 새로운 데이터 셋
+                  setMaxPage({ num: Math.ceil(tmp.length / 6) }); // 검색 결과 -> 새로운 데이터 셋
                 }}
               >
                 <MdSearch />
@@ -118,10 +123,28 @@ const Key = () => {
             </div>
           </div>
           {/* 리스트 */}
-          {six.map((v, i) => (
-            //<Normal idx={i} car={v} />
-            <Modify idx={i} car={v} ku={ku} />
-          ))}
+          {six.map((v, i) => {
+            // 해당 index 의 컴포넌트는 정보 수정 중인 경우
+            return i !== mIdx ? (
+              <Normal
+                idx={i}
+                car={v}
+                ku={ku}
+                mIdx={mIdx}
+                setMIdx={setMIdx}
+                setMaxPage={setMaxPage}
+              />
+            ) : (
+              <Modify
+                page={page}
+                idx={i}
+                car={v}
+                ku={ku}
+                setMIdx={setMIdx}
+                setMaxPage={setMaxPage}
+              />
+            );
+          })}
         </div>
         {/* 페이지 */}
         <div className="w-[300px] flex justify-between items-center mt-10">
@@ -135,22 +158,21 @@ const Key = () => {
           />
           <div className="text-lg">
             Page <span className="text-2xl">{page}</span> of{" "}
-            <span className="text-2xl">{maxPage}</span>
+            <span className="text-2xl">{maxPage.num}</span>
           </div>
           <MdArrowForward
             className="text-5xl"
             onClick={() => {
-              if (page === maxPage) return;
+              if (page === maxPage.num) return;
               setSix(ku.fillSix(ku.getPageKeys(page + 1)));
               setPage(page + 1);
             }}
           />
         </div>
       </div>
+      {alertInfo.state ? <Alert /> : null}
     </>
   );
 };
 
 export default Key;
-
-/**/
