fda2cbd6b714d23148941e53af729a7f8f4a9355
yh-project <yoha6865@gmail.com>
2023-11-10 20:00:50 +0900

    Feat/#22: 차량 상태 관리 atom 추가
    차량 상태 관리에서 필요한 데이터들 관리 atom 추가
     - 최대 페이지, 현재 페이지, 차량 리스트...
     - 데이터 수정 또한 여기서 이루어짐

---
+++ src/recoil/carStateAtom.js
@@ -0,0 +1,30 @@
+import { atom, selector } from "recoil";
+
+export const carStateAtom = atom({
+  key: "carStateAtom",
+  default: {
+    cars: [
+      {
+        carName: "",
+        carNumber: "",
+        carState: "",
+        carId: -1,
+      },
+    ], // 차량 리스트
+    maxPage: 0, // 최대 페이지
+    page: 0, // 현재 페이지
+    filterMenu: ["전체", "사용가능", "수리/점검", "도난"], // 검색 필터 메뉴
+    stateMenu: ["사용가능", "수리/점검", "도난"], // 상태 메뉴
+  },
+});
+
+export const infoSelector = selector({
+  key: "infoSelector",
+  get: ({ get }) => get(carStateAtom),
+  set: ({ set, get }, newInfo) => {
+    const before = { ...get(carStateAtom) };
+    const tmp = Object.keys(newInfo);
+    tmp.forEach((v) => (before[v] = newInfo[v]));
+    set(carStateAtom, before);
+  },
+});
